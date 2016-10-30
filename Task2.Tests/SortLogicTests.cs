939a49103c0fac657c3f028f2b3ef42673edd5f2
Илья <wiks30@gmail.com>
2016-10-30 20:45:56 +0300

    add Task2

---
+++ Task2.Tests/SortLogicTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using static Task2.SortLogic;
+
+namespace Task2.Tests
+{
+    [TestFixture]
+    public class SortLogicTests
+    {
+
+        [TestCase(arg1: new int[][]{new[]{1,-2,3},new[]{0,2,13,4},new[]{1,3},new[]{14}}, arg2: new method(Sum),ExpectedResult = 0)]              
+        public int Sort_SortJaggedArrayWithFewSolutions(int[][] array, Func<int[], int[], bool, bool> func,bool invert = false)
+        {
+            string str = string.Empty;
+            foreach (var item in Sort(array, func, invert))
+            {
+                str = item.Aggregate(string.Empty, (current, number) => current + number.ToString());
+            }
+            Console.WriteLine(str);
+
+            return 0;
+        }
+    }
+}
---
+++ Task2/SortLogic.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Linq;
+
+namespace Task2
+{
+    public static class SortLogic
+    {
+        public delegate bool method(int[] first, int[] second, bool invert);
+
+
+        public static int[][] Sort(int[][] array, Func<int[], int[], bool, bool> func, bool invert = false)
+        {
+            if(ReferenceEquals(array, null) || ReferenceEquals(func, null))
+                throw new NullReferenceException();
+
+            for (var i = 0; i < array.Length; i++)
+            {
+                for (var j = 0; j < array.Length - i - 1; j++)
+                {
+                    if (!func(array[j], array[j + 1], invert)) continue;
+                    var temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+
+            return array;
+        }
+
+        public static bool Sum(int[] first, int[] second, bool invert)
+        {
+            if (invert)
+            {
+                return first.Sum() > second.Sum();
+            }
+            else
+            {
+                return first.Sum() < second.Sum();
+            }
+
+        }
+
+
+        public static bool Min(int[] first, int[] second, bool invert)
+        {
+            if (invert)
+            {
+                return first.Min() > second.Min();
+            }
+            else
+            {
+                return first.Min() < second.Min();
+            }
+
+        }
+
+        public static bool Max(int[] first, int[] second, bool invert)
+        {
+            if (invert)
+            {
+                return first.Max() > second.Max();
+            }
+            else
+            {
+                return first.Max() < second.Max();
+            }
+
+        }
+    }
+}
