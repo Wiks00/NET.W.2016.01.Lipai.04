ddefe48b0581fd95f692a0d6b9a3356ba1604a60
Илья <wiks30@gmail.com>
2016-10-31 20:46:05 +0300

    bla

---
+++ Task1.Tests/GCDLogicTests.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using static Task1.GCDLogic;
+
+namespace Task1.Tests
+{
+    [TestFixture]
+    public class GCDLogicTests
+    {
+        private object a = (object)new MyTimerDelegate(Gcd);
+
+        [TestCase(30, 18,ExpectedResult = 6)]
+        public int Gcd_FindGreatestCommonDivisor(int par,int par2)
+        {
+            return Gcd(par, par2);
+        }
+
+        [TestCase(5, 10, 0, ExpectedResult = 5)]
+        public int Gcd_FindGreatestCommonDivisor(int par, int par2,int par3)
+        {
+            return Gcd(par, par2,par3);
+        }
+
+        [TestCase(5, 10, 1, 5,ExpectedResult = 1)]
+        public int Gcd_FindGreatestCommonDivisor(params int[] ingeres)
+        {
+            return Gcd(ingeres);
+        }
+
+        [TestCase(30, 18, ExpectedResult = 6)]
+        public int BinaryGcd_FindGreatestCommonDivisor(int par, int par2)
+        {
+            return BinaryGcd(par, par2);
+        }
+
+        [TestCase(5, 10, 0, ExpectedResult = 5)]
+        public int BinaryGcd_FindGreatestCommonDivisor(int par, int par2, int par3)
+        {
+            return BinaryGcd(par, par2, par3);
+        }
+
+        [TestCase(5, 10, 1, 5, ExpectedResult = 1)]
+        public int BinaryGcd_FindGreatestCommonDivisor(params int[] ingeres)
+        {
+            return BinaryGcd(ingeres);
+        }
+
+        private static readonly object[] sourceListsForSpeedTest = {new object[] {new MyTimerDelegate(Gcd), new[] { 5, 10, 1, 5 }},
+                                                                    new object[] {new MyTimerDelegate(Gcd), new[] { 30, 18 }},
+                                                                    new object[] {new MyTimerDelegate(BinaryGcd), new[] { 5, 10, 1, 5 }},
+                                                                    new object[] {new MyTimerDelegate(BinaryGcd), new[] { 30, 18 }}};
+
+        [Test, TestCaseSource(nameof(sourceListsForSpeedTest))]
+        public void TestSpeed_TestSpeed(MyTimerDelegate method,params int[] ints)
+        {
+            Console.WriteLine(ints.Length > 2
+                ? TestSpeed(method, ints).ToString()
+                : TestSpeed(method, ints[0], ints[1]).ToString());
+        }
+
+        [TestCase(-1, 18)]
+        public void Gcd_TestForArgumentOutOfRangeException(int par, int par2)
+        {
+            Assert.That(() => Gcd(par, par2), Throws.TypeOf<ArgumentOutOfRangeException>());
+        }
+
+        [TestCase(-1, 10, 0)]
+        [TestCase(5, 10, -1)]
+        public void Gcd_TestForArgumentOutOfRangeExceptiond(int par, int par2, int par3)
+        {
+            Assert.That(() => Gcd(par, par2, par3), Throws.TypeOf<ArgumentOutOfRangeException>());
+        }
+
+    }
+}
---
+++ Task1/GCDLogic.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    public class GCDLogic
+    {
+        /// <summary>
+        /// delegate for speed test
+        /// </summary>
+        /// <param name="integers">gcd of that integers</param>      
+        public delegate int MyTimerDelegate(params int[] integers);
+
+        /// <summary>
+        /// Calculete Greatest Common Divisor by Euclid rule
+        /// </summary>
+        /// <param name="a">first number</param>
+        /// <param name="b">second number</param>
+        /// <returns>Greatest Common Divisor of two numbers</returns>
+        /// <exception cref="ArgumentOutOfRangeException"></exception>
+        public static int Gcd(int a, int b)
+        {
+            if(a < 0 || b < 0)
+                throw new ArgumentOutOfRangeException();
+            int t;
+            while (b != 0)
+            {
+                t = b;
+                b = a % b;
+                a = t;
+            }
+            return a;
+        }
+
+        /// <summary>
+        /// Calculete Greatest Common Divisor by Euclid rule
+        /// </summary>
+        /// <param name="a">first number</param>
+        /// <param name="b">second number</param>
+        /// <param name="c">third number</param>
+        /// <returns>Greatest Common Divisor of three numbers</returns>
+        /// <exception cref="ArgumentOutOfRangeException"></exception>
+        public static int Gcd(int a, int b, int c)
+        {
+            if(c < 0)
+                throw new ArgumentOutOfRangeException();
+
+            return Gcd(Gcd(a, b), c);
+        }
+
+        /// <summary>
+        /// Calculete Greatest Common Divisor by Euclid rule
+        /// </summary>
+        /// <param name="integers">array of numbers</param>
+        /// <returns>Greatest Common Divisor of array of numbers</returns>
+        /// <exception cref="ArgumentOutOfRangeException"></exception>
+        public static int Gcd(params int[] integers)
+        {
+            if(integers.Length < 0 || integers.Any(curent => curent < 0))
+                throw new ArgumentOutOfRangeException();
+
+            int value = integers[0];
+            for (int i = 1; i < integers.Length -1; i++)
+            {
+                value = Gcd(value, integers[i]);
+            }
+            return Gcd(value, integers[integers.Length -1]);
+        }
+
+        /// <summary>
+        /// Calculete Greatest Common Divisor by binary Euclid rule
+        /// </summary>
+        /// <param name="a">first number</param>
+        /// <param name="b">second number</param>
+        /// <returns>Greatest Common Divisor of two numbers</returns>
+        /// <exception cref="ArgumentOutOfRangeException"></exception>
+        public static int BinaryGcd(int a, int b)
+        {
+            if (a < 0 || b < 0)
+                throw new ArgumentOutOfRangeException();
+
+            if (a == 0) return b;
+            if (b == 0) return a;
+            if (a == b) return a;
+            if (a == 1 || b == 1) return 1;
+            if ((a % 2 == 0) && (b % 2 == 0)) return 2 * BinaryGcd(a / 2, b / 2);
+            if ((a % 2 == 0) && (b % 2 != 0)) return BinaryGcd(a / 2, b);
+            if ((a % 2 != 0) && (b % 2 == 0)) return BinaryGcd(a, b / 2);
+            if ((a % 2 != 0) && (b % 2 != 0) && b > a) return BinaryGcd(a, (b - a) / 2);
+            return BinaryGcd((a - b) / 2, b);
+        }
+
+        /// <summary>
+        /// Calculete Greatest Common Divisor by binary Euclid rule
+        /// </summary>
+        /// <param name="a">first number</param>
+        /// <param name="b">second number</param>
+        /// <param name="c">third number</param>
+        /// <returns>Greatest Common Divisor of three numbers</returns>
+        /// <exception cref="ArgumentOutOfRangeException"></exception>
+        public static int BinaryGcd(int a, int b,int c)
+        {
+            if (c < 0)
+                throw new ArgumentOutOfRangeException();
+
+            return BinaryGcd(BinaryGcd(a, b), c);
+        }
+
+        /// <summary>
+        /// Calculete Greatest Common Divisor by binary Euclid rule
+        /// </summary>
+        /// <param name="integers">array of numbers</param>
+        /// <returns>Greatest Common Divisor of array of numbers</returns>
+        /// <exception cref="ArgumentOutOfRangeException"></exception>
+        public static int BinaryGcd(params int[] integers)
+        {
+            if (integers.Length < 0 || integers.Any(curent => curent < 0))
+                throw new ArgumentOutOfRangeException();
+
+            int value = integers[0];
+            for (int i = 1; i < integers.Length - 1; i++)
+            {
+                value = BinaryGcd(value, integers[i]);
+            }
+            return BinaryGcd(value, integers[integers.Length - 1]);
+        }
+
+        /// <summary>
+        /// Test speed of method
+        /// </summary>
+        /// <param name="method">Tested method</param>
+        /// <param name="a">first parameter for method</param>
+        /// <param name="b">second parameter for method</param>
+        /// <returns>Ticks count</returns>
+        /// <exception cref="NullReferenceException"></exception>
+        /// <exception cref="ArgumentOutOfRangeException"></exception>
+        public static long TestSpeed(MyTimerDelegate method, int a, int b)
+        {
+            if(ReferenceEquals(method,null))
+                throw new NullReferenceException();
+            if(a < 0 || b < 0)
+                throw new ArgumentOutOfRangeException();
+
+            Stopwatch watch = new Stopwatch();
+            watch.Start();
+            method(a,b);
+            watch.Stop();
+            return watch.ElapsedTicks;
+        }
+
+        /// <summary>
+        /// Test speed of method
+        /// </summary>
+        /// <param name="method">Tested method</param>
+        ///<param name="integers">array of parameters</param>
+        /// <returns>Ticks count</returns>
+        /// <exception cref="NullReferenceException"></exception>
+        /// <exception cref="ArgumentOutOfRangeException"></exception>
+        public static long TestSpeed(MyTimerDelegate method, params int[] integers)
+        {
+            if (ReferenceEquals(method, null))
+                throw new NullReferenceException();
+            if (integers.Length < 0 || integers.Any(curent => curent < 0))
+                throw new ArgumentOutOfRangeException();
+
+            Stopwatch watch = new Stopwatch();
+            watch.Start();
+            method(integers);
+            watch.Stop();
+            return watch.ElapsedTicks;
+        }
+    }
+}
